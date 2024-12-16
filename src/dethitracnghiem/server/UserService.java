d6ad81eabc7cef5f5a8f2a615b8b2d717b045cbf
Thanh762003 <lephuocthanhlong@gmail.com>
2024-12-16 16:13:57 +0000

    Registration Users

---
+++ src/dethitracnghiem/server/UserService.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dethitracnghiem.server;
+
+import java.sql.*;
+
+/**
+ *
+ * @author USER
+ */
+public class UserService {
+    public boolean dangKy(String email, String password, String hoTen, String gioiTinh, Date ngaySinh, String soDienThoai) {
+        Connection conn = null;
+    }
+}
