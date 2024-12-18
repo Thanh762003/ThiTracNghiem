aed238521d3cb7828cc4542aad09a21dd6932f89
JThanh2376 <jthanh2376@gmail.com>
2024-12-18 11:17:09 +0700

    Update Question

---
+++ src/dethitracnghiem/server/AdminEmailPassword.java
@@ -0,0 +1,15 @@
+package dethitracnghiem.server;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class AdminEmailPassword {
+    public static String email = "admin@gmail.com";
+    public static String password = "admin123";
+}
